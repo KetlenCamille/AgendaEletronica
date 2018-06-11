a6dd07cf24da46949d2a7e972cd1dd4a44de25dd
KetlenCamille <ketlen.rieper@gmail.com>
2018-06-11 10:14:35 -0300

    Criando a classe Usuario

---
+++ ProjetoAgenda/Models/Usuario.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Models
+{
+    class Usuario
+    {
+        public int IdUsuario { get; set; }
+
+        public string NomeUsuario { get; set; }
+
+        public string  cpfUsuario { get; set; }
+
+        public DateTime dataNascimentoUsuario { get; set; }
+
+        public string telefoneUsuario { get; set; }
+
+        public string emailUsuario { get; set; }
+
+        public string senhaUsuario { get; set; }
+    }
+}
