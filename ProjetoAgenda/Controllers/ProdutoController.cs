f0cf54d3b90cddc1907aa6c5dced0a9bfaa5f060
KetlenCamille <ketlen.rieper@gmail.com>
2018-06-11 11:00:27 -0300

    Implementando interface IBaseController no ProdutoController

--- ProjetoAgenda/Controllers/ProdutoController.cs
+++ ProjetoAgenda/Controllers/ProdutoController.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Controllers.Base;
+using Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,8 +8,31 @@ using System.Threading.Tasks;
 
 namespace Controllers
 {
-    public class ProdutoController
+    public class ProdutoController : IBaseController<Produto>
     {
+        public Produto BuscarPorId(int id)
+        {
+            throw new NotImplementedException();
+        }
 
+        public void Cadastrar(Produto produto)
+        {
+            throw new NotImplementedException();
+        }
+    
+        public void Editar(Produto entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Excluir(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IList<Produto> ListarTodos()
+        {
+            throw new NotImplementedException();
+        }
     }
 }
