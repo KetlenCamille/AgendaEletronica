ec9938126f24f6c571143648472be132df9d953b
KetlenCamille <ketlen.rieper@gmail.com>
2018-06-14 08:32:23 -0300

    Criando classe UsuarioController

---
+++ ProjetoAgenda/Controllers/UsuarioController.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Controllers.DAO;
+
+namespace Controllers
+{
+    public class UsuarioController
+    {
+        Contexto contexto = new Contexto();
+
+    }
+}
