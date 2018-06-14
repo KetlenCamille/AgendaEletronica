892a083fe59657579025a4d7befb67053f6113e0
KetlenCamille <ketlen.rieper@gmail.com>
2018-06-14 08:34:25 -0300

    Implementando a interface IBaseController no UsuarioControler

--- ProjetoAgenda/Controllers/UsuarioController.cs
+++ ProjetoAgenda/Controllers/UsuarioController.cs
@@ -3,13 +3,39 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Controllers.Base;
 using Controllers.DAO;
+using Models;
 
 namespace Controllers
 {
-    public class UsuarioController
+    public class UsuarioController : IBaseController<Usuario>
     {
         Contexto contexto = new Contexto();
 
+        public Usuario BuscarPorId(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Cadastrar(Usuario entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Editar(Usuario entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Excluir(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IList<Usuario> ListarTodos()
+        {
+            throw new NotImplementedException();
+        }
     }
 }
