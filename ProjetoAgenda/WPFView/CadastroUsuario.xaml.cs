9124f398fef704edae17bbac8ce41b43614d2399
KetlenCamille <ketlen.rieper@gmail.com>
2018-06-21 11:23:46 -0300

    Adicionando tryCatch() no CadastroUsuario.xaml.cs

--- ProjetoAgenda/WPFView/CadastroUsuario.xaml.cs
+++ ProjetoAgenda/WPFView/CadastroUsuario.xaml.cs
@@ -28,27 +28,35 @@ namespace WPFView
 
         private void buttonCadastrar_Click(object sender, RoutedEventArgs e)
         {
-            Usuario usuarioView = new Usuario();
-
-            usuarioView.NomeUsuario = nomeUsuario.Text;
-            usuarioView.cpfUsuario = cpfUsuario.Text;
-            usuarioView.dataNascimentoUsuario = datanascUsuario.SelectedDate.Value;
-            usuarioView.emailUsuario = emailUsuario.Text;
-            usuarioView.senhaUsuario = senhaUsuario.Text;
-
-            UsuarioController usuContr = new UsuarioController();
-            int resp = usuContr.Cadastrar(usuarioView);
-
-            if(resp == 1)
+            try
             {
-                MessageBox.Show("Cadastrado com Sucesso!");
-            }
-            else if (resp == 0)
-            {
-                MessageBox.Show("Houston, temos um problema");
-            }
+                Usuario usuarioView = new Usuario();
 
-            this.Close();
+                usuarioView.NomeUsuario = nomeUsuario.Text;
+                usuarioView.cpfUsuario = cpfUsuario.Text;
+                usuarioView.dataNascimentoUsuario = datanascUsuario.SelectedDate.Value;
+                usuarioView.emailUsuario = emailUsuario.Text;
+                usuarioView.senhaUsuario = senhaUsuario.Text;
+
+                UsuarioController usuContr = new UsuarioController();
+                int resp = usuContr.Cadastrar(usuarioView);
+
+                if (resp == 1)
+                {
+                    MessageBox.Show("Cadastrado com Sucesso!");
+                }
+                else if (resp == 0)
+                {
+                    MessageBox.Show("Houston, temos um problema!");
+                }
+
+                this.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Ligue para o suporte: " + ex);
+
+            } 
         }
     }
 }
