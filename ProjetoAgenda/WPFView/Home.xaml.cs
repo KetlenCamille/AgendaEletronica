3c3e872632dc2eba327c9a760fd72dfcd8204e2f
KetlenCamille <ketlen.rieper@gmail.com>
2018-06-28 11:59:41 -0300

    Alterações efetuadas na Home.xaml.cs para  a sincronização do Menu

--- ProjetoAgenda/WPFView/Home.xaml.cs
+++ ProjetoAgenda/WPFView/Home.xaml.cs
@@ -48,21 +48,41 @@ namespace WPFView
             ButtonCloseMenu.Visibility = Visibility.Collapsed;
         }
 
-        private void TabControl_SelectionChanged(object sender, SelectionChangedEventArgs e)
-        {
-            UsuarioController usuario = new UsuarioController();
-            dgEstabBanco.ItemsSource = usuario.ListarTodos();
-        }
-
         private void ListCategoria(object sender, SelectionChangedEventArgs e)
         {
-            if(bancoItem.IsSelected)
+            EstabelecimentoController estabelecimentoController = new EstabelecimentoController();
+            string categoria;
+            if (bancoItem.IsSelected)
             {
-                string categoria = "Banco";
-                EstabelecimentoController estabelecimentoController = new EstabelecimentoController();
+                categoria = "Banco";
                 dgBanco.ItemsSource = estabelecimentoController.ListarPorCategoria(categoria);
             }
-            
+            else if (estacionamentoItem.IsSelected)
+            {
+                categoria = "Estacionamento";
+                dgEstacionamento.ItemsSource = estabelecimentoController.ListarPorCategoria(categoria);
+            }
+            else if (farmaciaItem.IsSelected)
+            {
+                categoria = "Farmácia";
+                dgFarmacia.ItemsSource = estabelecimentoController.ListarPorCategoria(categoria);
+            }
+            else if (fastfoodItem.IsSelected)
+            {
+                categoria = "Fast-Food";
+                dgFastfood.ItemsSource = estabelecimentoController.ListarPorCategoria(categoria);
+            }
+            else if (hotelItem.IsSelected)
+            {
+                categoria = "Hotel";
+                dgHotel.ItemsSource = estabelecimentoController.ListarPorCategoria(categoria);
+            }
+            else if (petshopItem.IsSelected)
+            {
+                categoria = "Pet-Shop";
+                dgPetshop.ItemsSource = estabelecimentoController.ListarPorCategoria(categoria);
+            }
+
         } 
 
         private void alterarDadosConta_Click(object sender, RoutedEventArgs e)
@@ -70,5 +90,62 @@ namespace WPFView
             configConta configConta = new configConta();
             configConta.Show();
         }
+
+        private void PackIcon_MouseUpBanco(object sender, MouseButtonEventArgs e)
+        {
+            menuTab.SelectedIndex = bancoItem.TabIndex;
+            menuTab.SelectedItem = bancoItem;
+            bancoItem.IsSelected = true;
+        }
+
+        private void PackIcon_MouseUpDestaques(object sender, MouseButtonEventArgs e)
+        {
+            menuTab.SelectedIndex = destaquesItem.TabIndex;
+            menuTab.SelectedItem = destaquesItem;
+            destaquesItem.IsSelected = true;
+        }
+
+        private void PackIcon_MouseUpPesquisar(object sender, MouseButtonEventArgs e)
+        {
+            menuTab.SelectedIndex = pesquisarItem.TabIndex;
+            menuTab.SelectedItem = pesquisarItem;
+            pesquisarItem.IsSelected = true;
+        }
+
+        private void PackIcon_MouseUpEstacionamento(object sender, MouseButtonEventArgs e)
+        {
+            menuTab.SelectedIndex = estacionamentoItem.TabIndex;
+            menuTab.SelectedItem = estacionamentoItem;
+            estacionamentoItem.IsSelected = true;
+        }
+
+        private void PackIcon_MouseUpFarmacia(object sender, MouseButtonEventArgs e)
+        {
+            menuTab.SelectedIndex = farmaciaItem.TabIndex;
+            menuTab.SelectedItem = farmaciaItem;
+            farmaciaItem.IsSelected = true;
+        }
+
+        private void PackIcon_MouseUpFastFood(object sender, MouseButtonEventArgs e)
+        {
+            menuTab.SelectedIndex = farmaciaItem.TabIndex;
+            menuTab.SelectedItem = fastfoodItem;
+            fastfoodItem.IsSelected = true;
+        }
+
+        private void PackIcon_MouseUpHotel(object sender, MouseButtonEventArgs e)
+        {
+            menuTab.SelectedIndex = hotelItem.TabIndex;
+            menuTab.SelectedItem = hotelItem;
+            hotelItem.IsSelected = true;
+        }
+
+        private void PackIcon_MouseUpPetShop(object sender, MouseButtonEventArgs e)
+        {
+            menuTab.SelectedIndex = petshopItem.TabIndex;
+            menuTab.SelectedItem = petshopItem;
+            petshopItem.IsSelected = true;
+        }
+
     }
 }
